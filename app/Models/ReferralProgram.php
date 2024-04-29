26271b7de8ef869dd5324dd313985815b8762059
Fernando Fernandes <fernando@parceirogoogle.com.br>
2024-04-29 09:42:40 -0300

    up

---
+++ app/Models/ReferralProgram.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ReferralProgram extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+       'commission',
+       'hold_amount',
+       'guideline',
+       'Reffral_enabled'
+
+    ];
+}
+
